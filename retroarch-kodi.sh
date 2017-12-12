604fd38b432e1ea61dae0da1d97a740ff80abb7e
ToKe79 <tomas.kelemen@gmail.com>
2017-12-12 20:32:23 +0100

    new core: gearboy

--- retroarch-kodi.sh
+++ retroarch-kodi.sh
@@ -32,7 +32,7 @@ PACKAGES_TOOLS="joyutils"
 PACKAGES_NETWORK="sixpair"
 PACKAGES_WAYLAND="libxkbcommon"
 PACKAGES_SYSUTILS="empty"
-PACKAGES_LIBRETRO="retroarch retroarch-assets retroarch-joypad-autoconfig retroarch-overlays libretro-database core-info glsl-shaders 2048 4do 81 atari800 beetle-lynx beetle-ngp beetle-pce beetle-pcfx beetle-saturn beetle-supergrafx beetle-vb beetle-wswan bluemsx cap32 chailove citra crocods desmume dinothawr dolphin dosbox easyrpg fbalpha fceumm fuse-libretro gambatte genesis-plus-gx gpsp gw-libretro handy hatari lutro mame2003 mame2003-midway melonds meowpc98 mgba mrboom mupen64plus nestopia nxengine o2em openlara parallel-n64 pcsx_rearmed picodrive pocketcdg ppsspp prboom prosystem puae px68k redream reicast sameboy scummvm snes9x snes9x2002 snes9x2005 snes9x2010 stella tgbdual tyrquake uae4arm uzem vbam vecx vice virtualjaguar xrick yabause"
+PACKAGES_LIBRETRO="retroarch retroarch-assets retroarch-joypad-autoconfig retroarch-overlays libretro-database core-info glsl-shaders 2048 4do 81 atari800 beetle-lynx beetle-ngp beetle-pce beetle-pcfx beetle-saturn beetle-supergrafx beetle-vb beetle-wswan bluemsx cap32 chailove citra crocods desmume dinothawr dolphin dosbox easyrpg fbalpha fceumm fuse-libretro gambatte gearboy genesis-plus-gx gpsp gw-libretro handy hatari lutro mame2003 mame2003-midway melonds meowpc98 mgba mrboom mupen64plus nestopia nxengine o2em openlara parallel-n64 pcsx_rearmed picodrive pocketcdg ppsspp prboom prosystem puae px68k redream reicast sameboy scummvm snes9x snes9x2002 snes9x2005 snes9x2010 stella tgbdual tyrquake uae4arm uzem vbam vecx vice virtualjaguar xrick yabause"
 
 DISABLED_CORES_RPi="ppsspp uae4arm reicast"
 DISABLED_CORES_Slice="ppsspp uae4arm reicast"
